339c047dc240b965852a4b6366689fc3b8128514
Howl <howl@MacBook-Air-Howl.local>
2022-02-10 12:41:45 +0600

    Contact Book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddContact from "./components/AddContact/AddContact";
+import React, { useState } from "react";
+import ContactsList from "./components/ContactsList/ContactsList";
+import EditContacts from "./components/EditContacts/EditContacts";
 
 function App() {
+  let [contacts, setContacts] = useState([]);
+  let [editContact, setEditContact] = useState({});
+  let [isEdit, setIsEdit] = useState(false);
+
+  function handleNewContact(newContact) {
+    let newContactsArr = [...contacts];
+
+    newContactsArr.push(newContact);
+
+    setContacts(newContactsArr);
+  }
+
+  function handleDeleteContact(id) {
+    let newContactsArr = contacts.filter((item) => {
+      return item.id !== id;
+    });
+
+    setContacts(newContactsArr);
+  }
+
+  function handleEditIndex(index) {
+    setIsEdit(true);
+    setEditContact(contacts[index]);
+  }
+
+  function handleSaveEditedContact(newContact) {
+    let newContactsArr = contacts.map((item) => {
+      if (item.id === newContact.id) {
+        return newContact;
+      }
+      return item;
+    });
+
+    setContacts(newContactsArr);
+    setIsEdit(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddContact handleNewContact={handleNewContact} />
+      {isEdit ? (
+        <EditContacts
+          editContact={editContact}
+          handleSaveEditedContact={handleSaveEditedContact}
+        />
+      ) : null}
+      <ContactsList
+        contacts={contacts}
+        handleDeleteContact={handleDeleteContact}
+        handleEditIndex={handleEditIndex}
+      />
     </div>
   );
 }
